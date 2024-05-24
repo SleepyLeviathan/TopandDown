366905f7e18b25c28d2db041b0a45415de48c48f
Harry <harrismarinos6@gmail.com>
2024-05-24 17:33:56 +0300

    Fixed Grave Glitch
    Player now continues animation after grave's death

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
+<<<<<<< HEAD
 # Things To Add
 1.  
+=======
+# Stuff To Add
+- 
+
+# ADDED
+- Player now uses animation and hit after grave's death
+>>>>>>> f428a3a (Fixed Grave Glitch)
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
-@onready var grave = $"../Grave"
+
 @onready var weapon = $Weapon
 
 const SPEED = 270
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -12,6 +13,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	var input_position = Input.get_vector("a", "d","w", "s")
+	var grave = get_node_or_null("../Grave")
 
 	velocity = input_position * SPEED
 	move_and_slide()
@@ -19,6 +21,7 @@ func _physics_process(delta):
 	if (Input.is_action_just_pressed("mouse_left")):
 		print("You hit!")
 		weapon.hit()
-		grave.hit()
+		if(grave):
+			grave.hit()
 		# do animation
 		# hit object of mouse position
